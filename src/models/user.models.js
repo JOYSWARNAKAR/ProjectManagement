2ee994396a4419400f0095c7dedf6c14b9114bc4
[Joy Swarnakar] <[joyswarnakar3@gmail.com]>
2026-01-04 02:26:19 +0530

    add hooks , methods in userSchema and add bcrypt.js

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,5 +1,5 @@
 import mongoose, {Schema} from "mongoose";
-
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
     {
@@ -64,6 +64,16 @@ const userSchema = new Schema(
     },
 );
 
-mongoose.model("User", userSchema)
+// hooks
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password"))  return next()
+
+    this.password= await bcrypt.hash(this.password, 10)
+    next()
+})
+// methods
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
 
 export const User = mongoose.model("User", userSchema)
